cd0c7bdddb6d9c29a082846ba9ff76cac6f73b58
Mitchell Doody-Burras <23240401@student.uwa.edu.au>
2023-08-29 22:14:12 +0800

    Transfered changes to .py version for server

--- project/downloadtenders.py
+++ project/downloadtenders.py
@@ -29,17 +29,27 @@ BUTTON_LOCATORS  = {
 
 CUSTOM_USER_AGENT  = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
 
-def download_multiple_tenders(max_workers, tender_dict, path):
+def batch_request_tenders(max_workers, batch_size, batch_interval, tender_dict, path):
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        futures = []        
-        for ref, link in tender_dict.items():
-            future = executor.submit(download_tender, link, ref, path)
-            futures.append(future)
-            
-        with tqdm(total=len(futures), desc="Downloading Tenders", colour='green') as pbar:
-            for future in futures:
-                future.result()
+        total_batches = len(tender_dict) // batch_size + (1 if len(tender_dict) % batch_size > 0 else 0)
+        
+        with tqdm(total=total_batches, desc="Downloading Batches", colour='green') as pbar:
+            for i in range(0, len(tender_dict), batch_size):
+                batch_tenders = list(tender_dict.items())[i:i+batch_size]
+                futures = []
+                
+                for ref, link in batch_tenders:
+                    future = executor.submit(download_tender, link, ref, path)
+                    futures.append(future)
+      
                 pbar.update(1)
+                
+                if batch_interval > 0 and i + batch_size < len(tender_dict):
+                    time.sleep(batch_interval)
+
+def log_error(ref, e):
+    error_message = f"Error: {ref}, {e}"
+    logging.error(error_message)
     
 def download_tender(link, ref, path):
     options = Options()
@@ -53,17 +63,13 @@ def download_tender(link, ref, path):
     driver = webdriver.Chrome(options=options)
     open_link(driver, link, ref)
     
-    # wait for button to be clickable
     wait = WebDriverWait(driver, 10)
-
     buttons = ["Download Now", "Download for Information Only", "Download Documents"]
     for button in buttons:
-        if not click_button(driver, wait, button, ref):
-            driver.quit()
-            break
-        
+        click_button(driver, wait, button, ref)
+            
     # Wait for downloads to complete
-    time.sleep(15)
+    time.sleep(20)
     driver.quit()
 
 def open_link(driver, link, ref):
@@ -71,8 +77,7 @@ def open_link(driver, link, ref):
         try:
             driver.get(link)
         except Exception as e:
-            error_message = f"Error: {ref}, {e}"
-            logging.error(error_message)
+            log_error(ref, e)
             driver.quit()
             break
         return
@@ -83,25 +88,32 @@ def click_button(driver, wait, button_text, ref):
     if locator == "xpath":
         button_text = f"//input[@value='{button_text}']"
     
-    try:
-        button = wait.until(EC.element_to_be_clickable((locator, button_text)))
-        button.click()
-    except Exception as e:
-        error_message = f"Error: {ref}, {e}"
-        logging.error(error_message)
-        return False
-    return True
+    while True:
+        try:
+            button = wait.until(EC.element_to_be_clickable((locator, button_text)))
+            button.click()
+        except Exception as e:
+            log_error(ref, e)
+            driver.quit()
+            break
+        return
 
+def log_error(ref, e):
+    error_message = f"Error: {ref}, {e}"
+    logging.error(error_message)
+    
 def main():
     parser = argparse.ArgumentParser(description="Download Tenders")
     parser.add_argument("--max_workers", type=int, default=10, help="Maximum number of worker threads")
+    parser.add_argument("--batch_size", type=int, default=5, help="Number of requests per batch")
+    parser.add_argument("--batch_interval", type=int, default=10, help="Time inbetween batch requests")
     parser.add_argument("--path", type=str, default="./downloads", help="Path to save downloaded files")
     args = parser.parse_args()
 
-    download_multiple_tenders(args.max_workers, ProTenders, args.path)
+    batch_request_tenders(args.max_workers, args.batch_size, args.batch_interval, ProTenders, args.path)
 
 if __name__ == "__main__":
     main()
     
 # how to run
-# python downloadtenders.py --max_workers 10 --path /path/to/save/downloads
+# python downloadtenders.py --max_workers 10 --batch_size 5 --batch_interval 10 --path /path/to/save/downloads
