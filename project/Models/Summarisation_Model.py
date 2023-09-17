a20ebe9e6525d6b2e5547093d97781ba626f571b
Mitchell Doody-Burras <23240401@student.uwa.edu.au>
2023-09-17 11:20:39 +0800

    Combined tenders data and available extra information into corpus. Ref number is key

--- project/Models/Summarisation_Model.py
+++ project/Models/Summarisation_Model.py
@@ -3,35 +3,35 @@ import os
 import pandas as pd
 from bs4 import BeautifulSoup
 
-def list_files(current_zip, corpus, zip_file, full_path):
+def list_files(ref, corpus, zip_file, full_path):
     for file_info in zip_file.infolist():
         file_name = os.path.join(full_path, file_info.filename)
 
         if file_name.lower().endswith(('doc', 'docx')):
             # read doc function
             doc_data = read_doc(file_name)
-            add(corpus, current_zip, doc_data)
+            add(corpus, ref, doc_data)
 
         elif file_name.lower().endswith('pdf'):
            # read pdf function
            pdf_data = read_pdf(file_name)
-           add(corpus, current_zip, pdf_data)
+           add(corpus, ref, pdf_data)
                  
         elif file_name.lower().endswith(('jpg', 'png')):
             # ocr function
             image_data = ocr(file_name)
-            add(corpus, current_zip, image_data)
+            add(corpus, ref, image_data)
 
         #print(file_name)
 
-def zip_search(current_zip, corpus, file_path, full_path=""):
+def zip_search(ref, corpus, file_path, full_path=""):
     with zipfile.ZipFile(file_path, 'r') as zip_file:
         full_path = os.path.join(full_path, zip_file.filename)
-        list_files(current_zip, corpus, zip_file, full_path)
+        list_files(ref, corpus, zip_file, full_path)
         for file_info in zip_file.infolist():
             if file_info.filename.endswith('.zip'):
                 nested_zip_file = zip_file.open(file_info.filename)
-                zip_search(current_zip, corpus, nested_zip_file, full_path)
+                zip_search(ref, corpus, nested_zip_file, full_path)
 
 def rec_search(corpus, directory_path):
     file_count = 0
@@ -39,29 +39,29 @@ def rec_search(corpus, directory_path):
     for root, _, files in os.walk(directory_path):
         for file in files:
             file_count += 1
-            if file_count >= 10:  # test first 10 files
+            if file_count >= 500:  # test first 500 files
                 return    
 
             file_path = os.path.join(root, file)
             if file_path.endswith('.zip'):
                 parts = file_path.split(os.path.sep)
-                current_zip = parts[-1].split("-specification.zip")[0]
-                zip_search(current_zip, corpus, file_path)
+                ref = parts[-1].split("-specification.zip")[0]
+                zip_search(ref, corpus, file_path)
 
 def add_base(corpus, tenders_data):
     for index, row in tenders_data.iterrows():
-        ref = row["Reference Number"]
+        ref = str(row["Reference Number"])
         title = row["Contract Title"]
         desc = remove_html_tags(row["Description"])
 
         combined = f"{title}. {desc}"
         add(corpus, ref, combined)
 
-def add(corpus, current_zip, data):
-    if current_zip in corpus:
-        corpus[current_zip].append(data)
+def add(corpus, ref, data):
+    if ref in corpus:
+        corpus[ref].append(data)
     else:
-        corpus[current_zip] = [data]
+        corpus[ref] = [data]
 
 def remove_html_tags(text):
     soup = BeautifulSoup(text, "html.parser")
@@ -86,20 +86,28 @@ def ocr(image_file):
     image_data = image_file
     return image_data
 
+def main(tenders_data_path, search_path):
+    corpus = {}
+
+    tenders_data = pd.read_excel(tenders_data_path)
+    tenders_data = tenders_data[["Reference Number", "Contract Title", "Description"]].dropna(subset=["Reference Number"]).drop_duplicates()
+
+    # add title and short description as base data
+    add_base(corpus, tenders_data)
+
+    # get available extra information from specification documents
+    rec_search(corpus, search_path)
+
+    return corpus
+
 # folder with zip files, not Tenders.zip
 # actually search recursively through everything, I think ...
-corpus = {}
-
 tenders_data_path = r"C:/Users/Mitch/git/UWACapstoneG2/data/UpdatedAgainTenders.xlsx"
-tenders_data = pd.read_excel(tenders_data_path)
-tenders_data = tenders_data[["Reference Number", "Contract Title", "Description"]].dropna(subset=["Reference Number"]).drop_duplicates()
-
-add_base(corpus, tenders_data)
-
-# search for extra information
 search_path = r"C:/Users/Mitch/Tenders"
-#rec_search(corpus, search_path)
 
-for key, value in corpus.items():
-    print(f"{key}: {value}")
-    print("<=============>")
+corpus = main(tenders_data_path, search_path)
+#for key, value in corpus.items():
+#    print(f"{key}: {value}")
+#    print("<=============>")
+
+print(corpus['BMW0104219'])
