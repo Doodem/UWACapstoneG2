0420588e26f7d64435f9a94b8b2464cb985cce88
Mitchell Doody-Burras <23240401@student.uwa.edu.au>
2023-09-17 09:51:26 +0800

    Included title and short description fields as base information per tender in the absence of specification documents

--- project/Models/Summarisation_Model.py
+++ project/Models/Summarisation_Model.py
@@ -1,5 +1,6 @@
 import zipfile
 import os
+import pandas as pd
 
 def list_files(current_zip, corpus, zip_file, full_path):
     for file_info in zip_file.infolist():
@@ -11,6 +12,7 @@ def list_files(current_zip, corpus, zip_file, full_path):
             add(corpus, current_zip, doc_data)
 
         elif file_name.lower().endswith('pdf'):
+           # read pdf function
            pdf_data = read_pdf(file_name)
            add(corpus, current_zip, pdf_data)
                  
@@ -19,7 +21,7 @@ def list_files(current_zip, corpus, zip_file, full_path):
             image_data = ocr(file_name)
             add(corpus, current_zip, image_data)
 
-        print(file_name)
+        #print(file_name)
 
 def zip_search(current_zip, corpus, file_path, full_path=""):
     with zipfile.ZipFile(file_path, 'r') as zip_file:
@@ -42,8 +44,17 @@ def rec_search(corpus, directory_path):
             file_path = os.path.join(root, file)
             if file_path.endswith('.zip'):
                 parts = file_path.split(os.path.sep)
-                current_zip = parts[-1]
+                current_zip = parts[-1].split("-specification.zip")[0]
                 zip_search(current_zip, corpus, file_path)
+
+def add_base(corpus, tenders_data):
+    for index, row in tenders_data.iterrows():
+        ref = row["Reference Number"]
+        title = row["Contract Title"]
+        desc = row["Description"]
+
+        combined = f"{title}. {desc}"
+        add(corpus, ref, combined)
 
 def add(corpus, current_zip, data):
     if current_zip in corpus:
@@ -71,8 +82,16 @@ def ocr(image_file):
 # folder with zip files, not Tenders.zip
 # actually search recursively through everything, I think ...
 corpus = {}
+
+tenders_data_path = r"C:/Users/Mitch/git/UWACapstoneG2/data/UpdatedAgainTenders.xlsx"
+tenders_data = pd.read_excel(tenders_data_path)
+tenders_data = tenders_data[["Reference Number", "Contract Title", "Description"]].dropna(subset=["Reference Number"]).drop_duplicates()
+
+add_base(corpus, tenders_data)
+
+# search for extra information
 search_path = r"C:/Users/Mitch/Tenders"
-rec_search(corpus, search_path)
+#rec_search(corpus, search_path)
 
 for key, value in corpus.items():
     print(f"{key}: {value}")
