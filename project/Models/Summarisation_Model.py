7235c875cd2a5926c04d7e77ef9b55c5d8a4b674
Max <max@Maxs-MacBook-Air.local>
2023-09-20 23:14:31 +0800

    get reading for doc and pdf pretty robust

--- project/Models/Summarisation_Model.py
+++ project/Models/Summarisation_Model.py
@@ -1,55 +1,118 @@
 import zipfile
+from zipfile import BadZipFile
+import io
 import os
+import tempfile
 import pandas as pd
 from bs4 import BeautifulSoup
 import PyPDF2
 from io import BytesIO    
 from docx import Document
+from pathlib import Path
+import textract
+import re
+from tqdm import tqdm
 
-def list_files(ref, corpus, zip_file, full_path):
-    for file_info in zip_file.infolist():
-        file_name = os.path.join(full_path, file_info.filename).replace('/', '\\')
+# READ FUNCTIONS GO HERE 
 
-        if file_name.lower().endswith(('doc', 'docx')):
-            # read doc function
-            doc_data = read_doc(file_name)
+docs_total = 0
+doc_read_failure = 0
+def read_doc(file_bytes, zip_file, nested_file_name):
+    global docs_total
+    global doc_read_failure
+    
+    docs_total += 1
+    content = None
+    # first try docx reader function
+    try:
+        doc = Document(io.BytesIO(file_bytes))
+        content = []
+        for paragraph in doc.paragraphs:
+            content.append(paragraph.text)
+        temp = "\n".join(content)
+        content = temp
+    except Exception as e:
+        content = None
+    
+    # then try textract and slow temp file writing
+    if content == None:
+        try: # try .doc first 
+            temp = tempfile.NamedTemporaryFile(suffix = ".doc")
+            temp.write(file_bytes)
+            content = textract.process(temp.name)
+        except Exception as e:
+            content = None
+            try: # then .docx..
+                temp2 = tempfile.NamedTemporaryFile(suffix = ".docx")
+                temp2.write(file_bytes)
+                content = textract.process(temp2.name)
+            except Exception as e:
+                # no dice
+                content = None
+            finally: 
+                temp2.close()
+        finally:
+            temp.close()
+            
+    if content != None:
+        return content
+    else: # nothing worked, so just report a doc read failure
+        doc_read_failure += 1
+        
+def read_pdf(file_bytes):
+    text = ""
+    pdf_reader = PyPDF2.PdfReader(io.BytesIO(file_bytes))
+    for page_number in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_number]
+        text += page.extract_text()
+    return text
+
+def ocr(image_file):
+    # extract text from image
+    image_data = image_file
+    return image_data
+
+bad_zips = 0
+def file_handler(ref, corpus, zip_file, nested_file_name):
+    global bad_zips
+    try:
+        nested_file_bytes = zip_file.read(nested_file_name)
+        if nested_file_name.lower().endswith(('doc', 'docx')):
+            doc_data = read_doc(nested_file_bytes, zip_file, nested_file_name)
             add(corpus, ref, doc_data)
+        elif nested_file_name.lower().endswith("pdf"):
+            pdf_data = read_pdf(nested_file_bytes)
+            add(corpus, ref, pdf_data)
+    except BadZipFile as badZipException:
+        bad_zips += 1
 
-        #elif file_name.lower().endswith('pdf'):
-           # read pdf function
-        #   pdf_data = read_pdf(file_name)
-        #   add(corpus, ref, pdf_data)
-                 
-        elif file_name.lower().endswith(('jpg', 'png')):
-            # ocr function
-            image_data = ocr(file_name)
-            add(corpus, ref, image_data)
+# ----------------------
 
-        #print(file_name)
-
-def zip_search(ref, corpus, file_path, full_path=""):
-    with zipfile.ZipFile(file_path, 'r') as zip_file:
-        full_path = os.path.join(full_path, zip_file.filename)
-        list_files(ref, corpus, zip_file, full_path)
+def zip_search(ref, corpus, zip_path, zip_data):
+    with zipfile.ZipFile(zip_data, 'r') as zip_file:
         for file_info in zip_file.infolist():
-            if file_info.filename.endswith('.zip'):
-                nested_zip_file = zip_file.open(file_info.filename)
-                zip_search(ref, corpus, nested_zip_file, full_path)
+            nested_file_name = file_info.filename
+            if nested_file_name.lower().endswith('.zip'):
+                try:
+                    nested_zip_data = io.BytesIO(zip_file.open(nested_file_name).read())
+                    nested_zip_path = os.path.join(zip_path, nested_file_name)
+                    zip_search(ref, corpus, nested_zip_path, nested_zip_data)
+                except BadZipFile as badZipException:
+                    bad_zips += 1
+            else:
+                file_handler(ref, corpus, zip_file, nested_file_name)
 
 def rec_search(corpus, directory_path):
-    file_count = 0
-
     for root, _, files in os.walk(directory_path):
-        for file in files:
-            file_count += 1
-            if file_count >= 500:  # test first 500 files
-                return    
-
-            file_path = os.path.join(root, file)
-            if file_path.endswith('.zip'):
-                parts = file_path.split(os.path.sep)
-                ref = parts[-1].split("-specification.zip")[0]
-                zip_search(ref, corpus, file_path)
+        zips_len = len(files)
+        with tqdm(total = zips_len, desc="Processing files", colour='green') as pbar:
+            for file in files:  
+                file_path = os.path.join(root, file)
+                if file_path.endswith('.zip'):
+                    parts = file_path.split(os.path.sep)
+                    ref = parts[-1].split("-specification.zip")[0]
+                    zip_search(ref, corpus, file_path, file_path)
+                pbar.update(1)
 
 def add_base(corpus, tenders_data):
     for index, row in tenders_data.iterrows():
@@ -72,40 +135,20 @@ def remove_html_tags(text):
     cleaned = ' '.join(cleaned.split())
     return cleaned
 
-# insert all read functions here per file type
 
-def read_doc(docx_file_path):
-    #extract text from doc
-    zip_file_path, docx_file = os.path.split(docx_file_path)
-
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_file:
-        docx_content = zip_file.read(docx_file)
-
-    doc = Document(BytesIO(docx_content))
-    content = []
-
-    for paragraph in doc.paragraphs:
-        content.append(paragraph.text)
-
-    text = "\n".join(content)
-    return text
-    
-def read_pdf(pdf_file_path):
-    #extract text from pdf
-    zip_file_path, pdf_file = os.path.split(pdf_file_path)
-    
-    text = ""
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_file:
-        pdf_reader = PyPDF2.PdfReader(BytesIO(zip_file.read(pdf_file))) 
-        for page_number in range(len(pdf_reader.pages)):
-            page = pdf_reader.pages[page_number]
-            text += page.extract_text()
+def clean_text(text):
+    # convert from binary string if needed
+    if type(text) == bytes:
+        text = text.decode("utf-8")
+    text = re.sub("[^a-zA-z0-9.,]", " ", text)
+    text = re.sub("\\\\", " ", text) 
+    text = re.sub("\s+", " ", text)
+    text = re.sub("\.+", ".", text)
     return text
 
-def ocr(image_file):
-    # extract text from image
-    image_data = image_file
-    return image_data
+def clean_corpus_dict(corpus):
+    for key in corpus:
+        corpus[key] = [clean_text(text) for text in corpus[key]]
 
 def main(tenders_data_path, search_path):
     corpus = {}
@@ -119,17 +162,21 @@ def main(tenders_data_path, search_path):
     # get available extra information from specification documents
     rec_search(corpus, search_path)
 
+    clean_corpus_dict(corpus)
+    
     return corpus
 
 # folder with zip files, not Tenders.zip
-# actually search recursively through everything, I think ...
-tenders_data_path = r"C:/Users/Mitch/git/UWACapstoneG2/data/UpdatedAgainTenders.xlsx"
-search_path = r"C:/Users/Mitch/Tenders"
+tenders_data_path = r"/Users/max/Documents/CapStone/UWACapstoneG2/data/UpdatedAgainTenders.xlsx"
+search_path = r"/Users/max/Documents/CapStone/data/tenders/"
 
 corpus = main(tenders_data_path, search_path)
 #for key, value in corpus.items():
 #    print(f"{key}: {value}")
+#    print(type(value))
 #    print("<=============>")
-
-print(corpus['BMW0104219'])
-
+#    break
+    
+print(docs_total)
+print(doc_read_failure)
+print(f"bad zips: {bad_zips}")
